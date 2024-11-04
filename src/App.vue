5b24f844f411902c01c36b3da7b664727ed79807
Lwin Phyo <lwinphyo271395@gmail.com>
2024-11-04 15:09:10 +0700

    commit 1

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <p>HELLO THERE</p>
+  <sign-up></sign-up>
+  <p>HI ANYONE??</p>
+
+</template>
+
+<script>
+import SignUp from './components/SignUp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SignUp
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  background-color: #e7e4e4;
+  margin: 0;
+  font-size: large;
+}
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,98 @@
+<template>
+  <p>Sign Up Form</p>
+
+  <form>
+    <p>Hi this is updated?</p>
+    <p>why nochanges</p>
+    <label for="">Email</label>
+    <input type="email" required v-model="email">
+    <label for="">password</label>
+    <input type="password" required v-model="password" >
+
+    <label for="">User Role_</label>
+    <select v-model="roles">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <div>
+      <input type="checkbox" v-model="accept">
+      <label for="">Accept terms and conditions</label>
+    </div>
+    <br>
+      <label for="">Check Names</label><br>
+    <div>
+      <input type="checkbox" value="LP" v-model="names">
+      <label for="">Lwin Phyo</label><br>
+    </div>
+    <div>
+      <input type="checkbox" value="SSS" v-model="names">
+      <label for="">Soe Seeseim</label><br>
+    </div>
+    <div>
+      <input type="checkbox" value="WS" v-model="names">
+      <label for="">Warain Soe</label><br>
+    </div>
+
+  </form>
+  <p>Email - {{ email }}</p>
+  <p>Password - {{ password }}</p>
+  <p>Role - {{ role }}</p>
+  <p>Accept - {{ accept }}</p>
+  <p>names - {{ names }}</p>
+
+</template>
+
+
+<script>
+export default {
+  data(){
+    return{
+      email:"LWINPHYO@GMAIL.COM",
+      password:"",
+      role:"designer",
+      accept:true,
+      names:[]
+    }
+  }
+
+}
+</script>
+
+<style>
+
+    form{
+        max-width: 400px;
+        margin: 30px auto;
+        background: rgb(253, 253, 252);
+        text-align: left;
+        padding: 40px;
+        border-radius: 10px;
+    }
+    label{
+        color: #451f1f;
+        display: inline-block;
+        margin: 20px 0 15px;
+        font-size: 0.6em;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        font-weight: bold;
+    }
+    input{
+        display: block;
+        padding: 10px 6px;
+        width: 100%;
+        box-sizing: border-box;
+        border: none;
+        border-bottom: 1px solid #7d6767;
+        color: #555;
+    }
+    input[type="checkbox"]{
+      display: inline-block;
+      width: 16px;
+      margin: 0 8px 0 0;
+      position: relative;
+      top: 2px;
+    }
+
+</style>
